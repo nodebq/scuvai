baa7121aaadac0d95bed445acf2c4580e6a88baa
baiqi <momoxinshang@gmail.com>
2018-04-28 18:25:56 +0800

    update

--- controllers/upload.js
+++ controllers/upload.js
@@ -4,6 +4,7 @@ var conn = require('../libs/mysql.js');
 var multiparty = require('multiparty');
 // var util = require('util');
 var fs = require('fs');
+var uuid = require('node-uuid');
 
 var upload = {
     name: "上传逻辑"
@@ -14,37 +15,40 @@ upload.up = function (req, res) {
     //生成multiparty对象，并配置上传目标路径
     //console.log('aaa');
     console.log('1');
-    var form = new multiparty.Form({uploadDir: '../uploads/'});
+    var form = new multiparty.Form({uploadDir: './uploads/'});
     //上传完成后处理
     form.parse(req, function (err, fields, files) {
-        //console.log(fields.type[0]);
+        // console.log(fields.type[0]);
+        console.log(files);
         var filesTmp =JSON.stringify(files, null, 2);//未用到?
         if (err) {
             console.log('parse error: ' + err);
         } else {
             console.log('parse files: ' + filesTmp);
-            var inputFile = files.inputFile[0];
-            var uploadedPath = inputFile.path;
-            var dstPath = '../avatar/' + inputFile.originalFilename;
+            // console.log(files);
+            // var inputFile = files.inputFile[0];
+            // var inputFile = 'inputFile';
+            // console.log(files.file[0].path);
+            var uploadedPath = files.file[0].path;
+            // console.log(files.file[0].originalFilename);
+            // var dstPath = './avatar/' + files.file[0].originalFilename;
+            var dstPath = './avatar/' + uuid.v1().replace(/-/g, "") + '.png';
             // var path = '..//' + inputFile.originalFilename;
             //重命名为真实文件名
             fs.rename(uploadedPath, dstPath, function (err) {
                 if (err) {
-                    //console.log('rename error: ' + err);
+                    console.log('rename error: ' + err);
                     console.log('404NotFound');
                     res.end(fyscu.out(code.NotFound404));
                     // return;
                 } else {
-                    //console.log('rename ok');
+                    console.log('rename ok');
                     // console.log('insert into clothes (userId,type,url,name) values ('+req.query.userId+','+fields.type[0]+','+path+','+inputFile.originalFilename+')');
-                    res.writeHead(200, {'content-type': 'text/plain;charset=utf-8'});
-                    res.write('received upload:\n\n');
                     res.end(fyscu.out(code.success));
                 }
             });
         }
         return 0;
-
     });
 };
 
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,8 +2,10 @@ $(function () {
     $("#upload").on("click", function () {//
         var url = 'http://localhost:2245/upload';
         var formData = new FormData();
-        console.log($("#inputFile")[0].files[0]);
+        // console.log($("#inputFile")[0].files[0]);
+        // var uploadName = $("#inputFile").attr("name");
         formData.append('file', $("#inputFile")[0].files[0]);
+        // formData.append("name",uploadName);
         $.ajax({
             url: url,
             async: true,//是否异步
@@ -14,18 +16,22 @@ $(function () {
             contentType: false,
             beforeSend: function () {
                 console.log("正在进行,请稍候");
-                $("upload").text("正在上传").attr('disabled','true');
+                $("#upload").text("正在上传").attr('disabled','true');
+                $("#inputFile").attr('disabled','true');
             },
             success: function (responseStr) {
-                if (responseStr.status === 0) {
-                    console.log("成功" + responseStr);
-                    $("upload").text("上传成功").attr('disabled','true');
+                if (responseStr.code === 200) {
+                    console.log("成功" , responseStr);
+                    $("#upload").text("上传成功").attr('disabled','true');
+                    $("#inputFile").attr('disabled','true');
                 } else {
-                    console.log("失败");
-                    $("upload").text("上传失败").attr('disabled','false');
+                    console.log("失败" , responseStr);
+                    $("#upload").text("上传失败").attr('disabled','false');
+                    $("#inputFile").attr('disabled','false');
                 }
             },
             error: function (responseStr) {
+                console.log(responseStr);
                 console.log("error");
             }
         });
